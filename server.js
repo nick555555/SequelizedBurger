e423b0f19ba8e4cb66bd5c3a5a9f9dcaf1b8418a
Nick <57273851+nick555555@users.noreply.github.com>
2020-02-21 18:16:55 -0800

    connecting to heroku

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-db.sequelize.sync({ force: false }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
   });

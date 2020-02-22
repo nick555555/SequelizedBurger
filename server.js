a93b1bb781938dabc3990da96af382633dcc341d
Nick <57273851+nick555555@users.noreply.github.com>
2020-02-21 18:03:31 -0800

    deleted orm references

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -23,7 +22,9 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
